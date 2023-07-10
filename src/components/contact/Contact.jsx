d72f359311363c9c1acc07c97022432f9a173641
mohamed <62141961+moohameed@users.noreply.github.com>
2023-07-10 12:02:44 +0000

    test de changement

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -53,7 +53,7 @@ const Contact = () => {
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
-          <button type ='submit'className="btn btn-primary">Send Message</button>
+          <button type ='submit'className="btn btn-primary"> test Message</button>
         </form>
 
       </div>
